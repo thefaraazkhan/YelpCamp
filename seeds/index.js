dd6849a44be3000daa351a99db5910e0a12b7544
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-05-12 22:06:15 +0530

    Added cluster map on main page

--- seeds/index.js
+++ seeds/index.js
@@ -16,7 +16,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 10; i++){ // do i<10 to 50 to return it to normal
+    for (let i = 0; i < 300; i++){ // do i<10 to 50 to return it to normal
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -27,7 +27,10 @@ const seedDB = async () => {
             price,
             geometry: { 
                 type: 'Point', 
-                coordinates: [ -73.9866, 40.7306 ] 
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ] 
             },
             images: [
                 {
